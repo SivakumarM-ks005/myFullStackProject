35ff1cf52f328770e121761d7a21cb6caa04958c
SivakumarM-ks005 <sivakumar.mani@kryasolutions.com>
2025-09-05 21:48:49 +0530

    updates
    updates

--- tcv-backend/index.js
+++ tcv-backend/index.js
@@ -3,11 +3,13 @@ var cors = require('cors');
 const app = express();
 const  connection = require('./connection');
 const userRouter = require('./routes/user');
-const categoryRouter = require('./routes/category')
+const categoryRouter = require('./routes/category');
+const productRouter = require('./routes/product')
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use('/api/user', userRouter);
 app.use('/api/category', categoryRouter);
+app.use('/api/product', productRouter);
 module.exports = app;
--- tcv-backend/routes/product.js
+++ tcv-backend/routes/product.js
@@ -3,3 +3,72 @@ const router = express.Router();
 const connection = require('../connection');
 const auth = require('../services/authentication');
 const checkRole = require('../services/checkRole');
+
+router.post('/add',auth.authenticateToken, checkRole.checkRole,(req,res)=>{
+    const product = req.body;
+    query = "insert into product (name,categoryId,description ,price,status) values (?,?,?,?,'true')";
+    connection.query(query,[product.name, product.categoryId, product.description, product.price],(err, results)=>{
+        if(!err){
+            return res.status(200).json({ message: "Record added successfully"});
+        }
+        else{
+            return res.status(500).json(err);
+        }
+    })
+});
+
+router.get('/get', auth.authenticateToken,(req, res, next)=>{
+    query = "select p.id, p.name, p.categoryId, p.price, p.status, c.id as categoryId, c.name as categoryName from product as p INNER JOIN category as c where p.categoryId = c.id";
+    connection.query(query,(err, results)=>{
+        if(!err){
+            return res.status(200).json(results);
+        }
+        else{
+            return res.status(500).json(err);
+        }
+    })
+});
+
+router.get('/getByCategoryId/:id', auth.authenticateToken,(res, results)=>{
+    const id = req.params.id;
+    query ="select id, name from product where categoryId =? and status='true'";
+    connection.query(query,[id], (err, results)=>{
+        if(!err){
+            return  res.status(200).json(results);
+        }
+        else{
+            return res.status(500).json(err);
+        }
+    })
+});
+
+router.get('/getById/:id', auth.authenticateToken,(req,res,next)=>{
+    const id = req.params.id;
+    query = "select id, name, description, price from product where id =?";
+    connection.query(query,[id],(err, results)=>{
+        if(!err){
+            return res.status(200).json(results[0]);
+        }
+        else {
+            return res.status(500).json(err);
+        }
+    })
+});
+
+router.patch('/update', auth.authenticateToken, checkRole.checkRole,(req, res, next)=>{
+    const product = req.body;
+    query ="insert into product set name=?, categoryId=?, description=?, price=? where id=?";
+    connection.query(query,[product.name, product.categoryId, product.description, product.price, product.id],(err, results)=>{
+        if(!err){
+            if(results.affectedRows==0){
+                return res.status(404).json({ message: "product does not fount"});
+            }
+            return res.status(200).json({ message: " product updated successfully"});
+        }
+        else {
+            return res.status(500).json(err);
+        }
+    })
+})
+
+module.exports = router
