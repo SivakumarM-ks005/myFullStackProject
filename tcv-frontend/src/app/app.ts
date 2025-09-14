9e8bb7537a9cebd93dd1f2de9f7777eb98eee759
SivakumarM-ks005 <sivakumar.mani@kryasolutions.com>
2025-09-14 11:35:36 +0530

    updates

--- tcv-frontend/src/app/app.ts
+++ tcv-frontend/src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgxUiLoaderConfigModule } from './shared/ngx-ui-loader-config.module';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    NgxUiLoaderConfigModule 
+],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
