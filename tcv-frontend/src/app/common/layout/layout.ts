0ff5b8b6497b199f325525fc3f4c05148034db3a
SivakumarM-ks005 <sivakumar.mani@kryasolutions.com>
2025-09-26 21:39:47 +0530

    logout
    logout

--- tcv-frontend/src/app/common/layout/layout.ts
+++ tcv-frontend/src/app/common/layout/layout.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { Router, RouterOutlet } from '@angular/router';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog'
@@ -10,64 +10,68 @@ import { Signup } from '../../signup/signup';
 import { ForgotPassword } from '../../forgot-password/forgot-password';
 import { Sidebar } from '../../sidebar/sidebar';
 // import { Login } from '../../login/login';
-import { User} from '../../services/user'
+import { User } from '../../services/user'
 import { response } from 'express';
+import { MatMenuModule } from '@angular/material/menu';
+import { Confirmation } from '../../dialog/confirmation/confirmation';
+
 @Component({
   selector: 'app-layout',
   imports: [
-    MatIconModule, 
+    MatIconModule,
     MatToolbarModule,
     MatButtonModule,
     MatSidenavModule,
     RouterOutlet,
-    Sidebar
-    
-],
+    Sidebar,
+    MatMenuModule
+  ],
   templateUrl: './layout.html',
   styleUrl: './layout.scss'
 })
 export class Layout {
   isMobile = false
   showSidebar = true;
-constructor( 
-  private oberser: BreakpointObserver,
-  private dialog: MatDialog,
-  private router: Router,
-  private userService : User
- ){}
-  toggleSidebar(){
-    this.showSidebar = this.showSidebar? false : true;
+  role: any
+  constructor(
+    private oberser: BreakpointObserver,
+    private dialog: MatDialog,
+    private router: Router,
+    private userService: User
+  ) { }
+  toggleSidebar() {
+    this.showSidebar = this.showSidebar ? false : true;
   }
- 
-  ngOnInit(){
-    this.oberser.observe([Breakpoints.Handset]).subscribe(result=>{
+
+  ngOnInit() {
+    this.oberser.observe([Breakpoints.Handset]).subscribe(result => {
       // console.log("result", result);
       this.isMobile = result.matches;
     });
-if (typeof window !== 'undefined' && localStorage) {
-    const token =localStorage.getItem('token');
-    console.log(token);
-    if(token != null){
-      this.userService.checkToken().subscribe((response:any)=>{
-        this.router.navigate(['/dashboards']);
-      },(error:any)=>{
-        
-        console.log(error);
-      })
-    }else {
-      this.router.navigate(['/']);
+    if (typeof window !== 'undefined' && localStorage) {
+      const token = localStorage.getItem('token');
+      console.log(token);
+      if (token != null) {
+        this.userService.checkToken().subscribe((response: any) => {
+          this.router.navigate(['/dashboards']);
+        }, (error: any) => {
+
+          console.log(error);
+        })
+      } else {
+        this.router.navigate(['/']);
+      }
     }
   }
-  }
 
-   signupAction(){
+  signupAction() {
     const dialogConfig = new MatDialogConfig();
     dialogConfig.width = "700px";
     this.dialog.open(Signup, dialogConfig);
   }
-  forgotPasswordAction(){
+  forgotPasswordAction() {
     const dialogConfig = new MatDialogConfig();
-    dialogConfig.width ="550px";
+    dialogConfig.width = "550px";
     this.dialog.open(ForgotPassword, dialogConfig);
   }
   // loginAction(){
@@ -75,4 +79,18 @@ if (typeof window !== 'undefined' && localStorage) {
   //   dialogConfig.width ="550px";
   //   this.dialog.open(Login,dialogConfig)
   // }
+
+  logout(){
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.data={
+      message:'Logout'
+    }
+    const dialogRef = this.dialog.open(Confirmation, dialogConfig);
+    const sub = dialogRef.componentInstance.onEmitStatusChange.subscribe((User)=>{
+      dialogRef.close();
+      localStorage.clear();
+      this.router.navigate(['/']);
+    })
+
+  }
 }
